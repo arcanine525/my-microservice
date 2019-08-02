98f68639bfa43cfd36cbddc2ff3001ca00ed070e
hiep.lh <hieplehoang05@gmail.com>
2019-08-02 10:47:33 +0700

    config traefik

---
+++ soceart/build/precache-manifest.05ffeacf91ca6b18fa20f7facf5a167b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b7338698a663ed1dc76302775f3ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd53aae969bd1dccadf4",
+    "url": "/static/css/main.8d9bbe34.chunk.css"
+  },
+  {
+    "revision": "8b57be918c4af2275133",
+    "url": "/static/js/2.753de65b.chunk.js"
+  },
+  {
+    "revision": "fd53aae969bd1dccadf4",
+    "url": "/static/js/main.8353cb3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03c86e1addaad5d4faa2bac73278bc21",
+    "url": "/static/media/CardItem.03c86e1a.png"
+  },
+  {
+    "revision": "998494c50e4a8c4577bdd80c69569263",
+    "url": "/static/media/StarsStripes.998494c5.ttf"
+  },
+  {
+    "revision": "c81877586e21c1851a7cc3fe4e5744a7",
+    "url": "/static/media/footer.c8187758.jpg"
+  },
+  {
+    "revision": "aefa29fb5f2c982df2abb556611510ed",
+    "url": "/static/media/header-background.aefa29fb.jpg"
+  },
+  {
+    "revision": "a63a9fb15be17b7351148960a3a89470",
+    "url": "/static/media/login-background.a63a9fb1.png"
+  },
+  {
+    "revision": "dea493b924817ec57ba1133d190a56f3",
+    "url": "/static/media/match-attax-logo.dea493b9.png"
+  },
+  {
+    "revision": "e88780470f5998a883d11759f4ed91b8",
+    "url": "/static/media/panini-logo.e8878047.png"
+  },
+  {
+    "revision": "3f1804442941525d7cb9b48dd45bce5d",
+    "url": "/static/media/soceart-logo.3f180444.svg"
+  }
+]);
